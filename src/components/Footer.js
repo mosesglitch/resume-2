9cdc989cbf079adbb54355bcfbbeac4ab9eafcc1
mosesglitch <mosesgachuru15@gmail.com>
2022-09-19 01:32:36 +0300

    add github pages 	modified:   package-lock.json 	modified:   package.json 	modified:   src/components/Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,7 +8,7 @@ import {
   MDBRow,
   MDBBtn,
 } from "mdb-react-ui-kit";
-// import "@fortawesome/fontawesome-free/css/all.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 import "bootstrap-css-only/css/bootstrap.min.css";
 import "mdbreact/dist/css/mdb.css";
 
